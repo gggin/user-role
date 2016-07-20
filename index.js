6dc3c7a58f9d9ece70736711f160523f97a3adab
Gin Liu <fm369o802340@163.com>
2016-07-20 17:07:27 +0800

    pure functions and utils and test

--- index.js
+++ index.js
@@ -2,45 +2,59 @@
  * Created by fm369 on 6/5/2016.
  */
 
-/*
-var user = {
-    name : "gggin", 
-    password : "123456",
-    salt : "xxxxx",
-    mobile : "17001105570",
-    registerTime : 124332432432,
-    email : "1123@163.com",
-    role : "xx",
-    extras : "{}"
-};
-*/
 var cryptor = require('oneline-crypto');
 var _ = require('lodash');
 
+function generateSHA256Password(salt, password) {
+    return cryptor.SHA256(salt + password);
+}
+
 function checkPassword(user, password) {
-    return cryptor.SHA256(user.salt + password) === user.password;
+    return generateSHA256Password(user.salt, password) === user.password;
 }
 
 function genSalt_() {
     return Math.random().toString(36).substring(7);
 }
 
-function createUser(user, salt) { // no salt and real password
-    var u = _.clone(user);
-    if (!u.name)
+function currentTimeNumber_() {
+    return Number(new Date);
+}
+
+function createUser(username, password, salt, registerTime) { // no salt and real password
+    if (!username)
         throw new Error('no user name');
-    if (!u.email)
-        throw new Error('no email');
-    if (!u.password)
+    if (!password)
         throw new Error('no password');
+    if (!salt)
+        throw new Error('no salt');
+    var u = {};
     u.salt = salt;
-    u.password = cryptor.SHA256(salt + u.password);
-    u.registerTime = Number(new Date);
+    u.password = generateSHA256Password(salt, password);
+    u.registerTime = registerTime;
     return u;
+}
+
+function changePasswordGenObject(salt, password) {
+    // return a new user object
+    var u = {};
+    u.salt = salt;
+    u.password = generateSHA256Password(salt, password);
+    return u;
+}
+
+function changePassword_(user, password) {
+    var t = changePasswordGenObject(genSalt_(), password);
+    user.salt = t.salt;
+    user.password = t.password;
 }
 
 module.exports = {
     checkPassword: checkPassword,
-    genSalt : genSalt_,
-    createUser: createUser
+    genSalt_: genSalt_,
+    createUser: createUser,
+    currentTimeNumber_: currentTimeNumber_,
+    changePasswordGenObject: changePasswordGenObject,
+    changePassword_: changePassword_,
+    generateSHA256Password: generateSHA256Password
 };
--- test.js
+++ test.js
@@ -5,7 +5,21 @@
 
 var role = require('./index');
 
-var salt = role.genSalt();
-var user = role.createUser({name:"gggin", password:'123', email:'123@163.com'}, salt);
+var salt = role.genSalt_();
+var user = role.createUser("gggin", '1235', salt, role.currentTimeNumber_());
+
+var r1 = role.checkPassword(user, '456');
+var r2 = role.checkPassword(user, '123');
+var r3 = role.checkPassword(user, '1235');
+
+console.log(user);
+
+role.changePassword_(user, "789");
+
+
+var r4 = role.checkPassword(user, '1235');
+var r5 = role.checkPassword(user, '789');
+
+console.log(r1, r2, r3, r4, r5);
 
 console.log(user);
